18f8c477f0cc2bd9639f1df6534bd22bc97a3d14
gitboy123455 <159932713+gitboy123455@users.noreply.github.com>
2025-03-29 19:59:48 +0530

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -2,3 +2,8 @@ import streamlit as web
 web.title("This is a test of streamlit!")
 web.header("This is a header!")
 web.subheader("This is a subheader!")
+if web.checkbox("Show/Hide"):
+  st.text("Checked")
+else:
+  st.text("Not checked")
+  
