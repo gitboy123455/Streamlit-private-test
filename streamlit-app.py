8eea320930dede0b705606d310d7fb4d9dbfe1b3
gitboy123455 <159932713+gitboy123455@users.noreply.github.com>
2025-03-29 20:02:32 +0530

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -3,7 +3,7 @@ web.title("This is a test of streamlit!")
 web.header("This is a header!")
 web.subheader("This is a subheader!")
 if web.checkbox("Show/Hide"):
-  st.text("Checked")
+  web.text("Checked")
 else:
-  st.text("Not checked")
+  web.text("Not checked")
   
